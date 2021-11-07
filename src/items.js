c67316cc43285bf1177a3f09d7ec3e526958d4af
weberadam12 <80291605+weberadam12@users.noreply.github.com>
2021-11-06 21:00:06 -0600

    Update items.js

--- src/items.js
+++ src/items.js
@@ -1,41 +1,49 @@
-let merch = [
+let items = [
   {
-    "id": 1,
-    "alt": "Dog Toy Tyre",
-    "img": "https://live.staticflickr.com/5093/5465309120_7ea2cd1e4f_b.jpg",
-    "item": "Puppy Tread",
-    "price": 15.25,
-    "description": "Rubber tire for your dog to play with.",
-    "quantity": 0
+    id: 1,
+    alt: "Time Machine",
+    img: "https://static2.srcdn.com/wordpress/wp-content/uploads/2021/06/Time-Machine-Phineas-And-Ferb.jpg?q=50&fit=crop&w=963&h=481&dpr=1.5",
+    item: "Time Machine",
+    price: 49999,
+    description: "The same time machine portrayed in season 1 episode \"It's About Time\"",
+    quantity: 0
   },
   {
-    "id": 2,
-    "alt": "Warm up Weights",
-    "img": "https://live.staticflickr.com/5011/5465299042_2d279f6e16_b.jpg",
-    "item": "Doggie Weights",
-    "price": 20.02,
-    "description": "Make sure your puppy is fit and thin with these puppy free weights.",
-    "quantity": 0
-  },
+    id: 2,
+    alt: "Shrinking Submarine",
+    img: "https://static3.srcdn.com/wordpress/wp-content/uploads/2021/06/Shrinking-Submarine-Phineas-And-Ferb.jpg?q=50&fit=crop&w=963&h=481&dpr=1.5",
+    item: "Shrinking Submarine",
+    price: 38765,
+    description: "Debued in season 1 episode \"Journey to the Center of Candace\"",
+      quantity: 0
+    },
   {
-    "id": 3,
-    "alt": "Still Life with Dog Toys",
-    "img": "https://live.staticflickr.com/3316/3177170536_a61b099d79_b.jpg",
-    "item": "Basket O' Balls",
-    "price": 49.99,
-    "description": "You dog can never have enough balls.",
-    "quantity": 0
-  },
+    id: 3,
+    alt: "Tree House",
+    img: "https://static1.srcdn.com/wordpress/wp-content/uploads/2021/06/Treehouse-Phineas-And-Ferb.jpg?q=50&fit=crop&w=963&h=481&dpr=1.5",
+    item: "Tree House Robots",
+    price: 120.99,
+    description: "From season 1 episode \"Tree To Get Ready\"",
+      quantity: 0
+    },
   {
-    "id": 4,
-    "alt": "Dog Clock",
-    "img": "https://media.istockphoto.com/photos/" +
-        "obedient-pet-training-smart-happy-puppy-picture-id1049354826?k=6&m=1049354826&s=612x612&w=0&h=g3TfPzRSQaTK_Rr941wz82tAEPY9muaVg2rQE6O9NXY=",
-    "item": "Dog Clock",
-    "price": 0.52,
-    "description": "Make sure you never miss play time with this thrifty nifty dog clock.",
-    "quantity": 0
-  }
+    id: 4,
+    alt: "Phinedroids and Ferbots",
+    img: "https://static3.srcdn.com/wordpress/wp-content/uploads/2021/06/Phinedroids-and-Ferbots-Phineas-And-Ferb.jpg?q=50&fit=crop&w=963&h=481&dpr=1.5",
+    item: "Phinedroids and Ferbots",
+    price: 52.89,
+    description: "Buy them by the pair from season 1 episode \"I, Brobot\"",
+      quantity: 0
+    },
+  {
+    id: 5,
+    alt: "Teleporters",
+    img: "https://static2.srcdn.com/wordpress/wp-content/uploads/2021/06/Teleporters-Phineas-And-Ferb.jpg?q=50&fit=crop&w=963&h=481&dpr=1.5",
+    item: "Teleporters",
+    price: 17489,
+    description: "From season 1 episode \"Does This Duckbill Make Me Look Fat?\"",
+      quantity: 0
+    }
 ]
 
-export default merch
+export default items;
