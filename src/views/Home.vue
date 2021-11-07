62da8d5865c929e7cce50afc297210166bdd222a
weberadam12 <80291605+weberadam12@users.noreply.github.com>
2021-11-06 21:01:22 -0600

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,54 +1,46 @@
 <template>
-  <div class="page-container">
+  <div class="page-container-home">
     <div class="content-wrap">
       <div>
         <div class="row  padded-row banner side-by-side">
           <div class="col">
             <p class="info-text">
-              Pamper your dog with the things they love.
+              Give your child the what they've always wanted.
             </p>
           </div>
           <div class="col text-center">
-            <img class="rounded" src="https://live.staticflickr.com/3433/3927529272_e6e5448807.jpg" alt="dog">
+            <img style="width = 500px" class="rounded" src="../assets/Theme-Song.jpg" alt="Phineas and Ferb">
           </div>
         </div>
 
         <div class="row padded-row infos">
           <div class="col">
             <div style="width: 18rem;">
-              <img style="height: 431.72px;" class="card-img-top" src="../assets/dog-training.jpg" alt="Card image cap">
+              <img style="width = 1.5rem;" class="card-img-top" src="../assets/About-to-Build.jpg" alt="Shopping link">
               <div class="card-body">
-                <h5 class="card-title">Training</h5>
-                <p class="card-text">Train your dog to do some cool tricks.</p>
-                <a href="https://www.google.com/search?q=dog+training&rlz=1C1CHBF_enUS850US850&oq=dog+training&aqs=
-                      chrome..69i57j0l4j46j0l2.1887j0j4&sourceid=chrome&ie=UTF-8" target="_blank" class="btn btn-primary">Learn More</a>
+                <p class="card-text">Buy already built and tested authentic Phineas and Ferb inventions</p>
+                <a href="/browse" target="_blank" class="btn btn-primary">Shop</a>
               </div>
+            <br>
             </div>
           </div>
           <div class="col">
             <div style="width: 18rem;">
-              <img class="card-img-top" src="../assets/doctor.jpg" alt="Card image cap">
+              <img style="width = 1.5rem;" class="card-img-top" src="../assets/Math-Tutor.jpg" alt="Card image cap" >
               <div class="card-body">
-                <h5 class="card-title">Dog Care</h5>
-                <p class="card-text">Learn from experts how to care for your little fur friend.</p>
-                <a  href="https://www.google.com/search?q=dog+care&rlz=1C1CHBF_enUS850US850&oq=dog+care&aqs=chrome..69i57
-                  j0l7.1015j0j7&sourceid=chrome&ie=UTF-8" target="_blank" class="btn btn-primary">Learn More</a>
+                <p class="card-text">Take some free math lessons from the boy genius Baljeet</p>
+                <a  href="https://www.youtube.com/watch?v=XsIes8UUjCk" target="_blank" class="btn btn-primary">Study Now</a>
               </div>
+              <br>
             </div>
           </div>
           <div class="col">
             <div style="width: 18rem;">
-              <img class="card-img-top" src="../assets/dog-date.jpg" alt="Card image cap">
+              <img style="width = 1.5rem;" class="card-img-top" src="../assets/FanFiction.jpg" alt="Card image cap" width="1.5rem">
               <div class="card-body">
-                <h5 class="card-title">Doggie Dates</h5>
-                <p class="card-text">Setup a doggie play date with a dog near you.</p>
-                <a href="https://www.google.com/search?safe=active&rlz=1C1CHBF_enUS850US850&sxsrf=ALeKk03gk75D8lCp5CP
-                      Lr9-VDH2sT8GlkA%3A1601044889500&ei=mQFuX8HvHdq0tAbfz6G4Dg&q=doggie+date&oq=doggie+date&gs_lcp=CgZwc3kt
-                      YWIQAzIECAAQCjIECAAQCjIKCC4QxwEQrwEQCjIECAAQCjIECAAQCjIECAAQCjIECAAQCjIECAAQCjICCAAyAggAOgQIABBHOgUIA
-                      BCxAzoFCAAQkgM6BAgjECc6BAgAEEM6CggAELEDEBQQhwI6BwgAEBQQhwI6CgguEMcBEKMCEEM6CwguELEDEMcBEKMCOggIABCxAxC
-                      DAToICC4QsQMQgwE6BwgAELEDEEM6BggAEAoQQzoCCC46CAguEMcBEK8BOgkIABCxAxAKEENKBQg3EgExSgUIJBIBMVDljAJY7qoCY
-                      OauAmgFcAN4AIABf4gBjAuSAQQyLjExmAEAoAEBqgEHZ3dzLXdpesgBCMABAQ&sclient=psy-ab&ved=0ahUKEwiBoKH_xITsAhVa
-                      Gs0KHd9nCOcQ4dUDCA0&uact=5" target="_blank" class="btn btn-primary">Learn More</a>
+                <p class="card-text">Discuss Phineas and Ferb fanfiction with other fanatics</p>
+                <a href="https://www.google.com/search?q=Phineas+and+Ferb+fan+fiction&source=lmns&bih=805&biw=855&rlz=1C1CHBF_enUS918US918&hl=
+                en&sa=X&ved=2ahUKEwiS25rs6IT0AhXMaM0KHVWKDi8Q_AUoAHoECAEQAA" target="_blank" class="btn btn-primary">Learn More</a>
               </div>
             </div>
           </div>
@@ -58,33 +50,24 @@
     </div>
 
     <div class="footer">
-      <hr>
-      <a href="https://github.com/bswheel/cp3.git">GitHub</a>
+
+      <a href="https://github.com/weberadam12/CreativeProject3.git">GitHub</a>
     </div>
 
-<!--    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
-<!--    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>-->
-<!--    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>-->
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
-//
-// export default {
-//   name: "Home",
-//   components: {
-//     HelloWorld
-//   }
-// };
-</script>
 
 <style>
   hr {
     height: 1px;
   }
-
+img.rounded {
+  height: 300px;
+}
+.card-img-top {
+  height: 1.5 rem;
+  width: auto;
+}
   .infos {
     margin: 50px;
   }
@@ -99,7 +82,7 @@
   .padded-row {
     padding-top: 50px;
     padding-bottom: 50px;
-    /*margin: auto;*/
+    margin: auto;
     width: 100%;
   }
 
@@ -111,19 +94,23 @@
   }
 
   .footer {
-    position: absolute;
+    text-align: center;
+    background-color: #173985;
+    color: #fff;
+    padding: 20px;
+    min-height: 50px;
+    position: fixed;
     bottom: 0;
     width: 100%;
-    text-align: center;
-    height: 100px;
-    padding-bottom: 50px;
-    background-color: #f8f9fa;
+    height: 2.5rem;
   }
 
-  .page-container {
-    position: relative;
-    min-height: 100vh;
-    width: 100%;
+  .page-container-home {
+  padding-top: 0;
+  padding-bottom: 0px;
+  text-align: center;
+  min-height: 0;
+  background-color: #7194E3
   }
 
   .content-wrap {
