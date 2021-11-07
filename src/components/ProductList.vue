f9a912c4f29259fd6d86e452243a7c190857e5ba
weberadam12 <80291605+weberadam12@users.noreply.github.com>
2021-11-06 21:01:59 -0600

    Update ProductList.vue

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,22 +1,23 @@
 <template>
-    <div class="wrapper">
-        <div class="products">
-            <div class="product" v-for="product in products" :key="product.id">
-                <div class="info">
-                    <h1>{{product.item}}</h1>
-                    <p>{{product.description}}</p>
-                </div>
-                <div class="image">
-                    <img :src="product.img" :alt="product.alt">
-                </div>
-                <div class="price">
-                    <h2>${{product.price}}</h2>
-                    <button @click="addToCart(product)" class="auto">Add to Cart</button>
-                </div>
-            </div>
-        </div>
+<div class="wrapper">
+  <div class="products">
+    <div class="product" v-for="product in products" :key="product.id">
+      <div class="info">
+        <h1>{{product.item}}</h1>
+        <p>{{product.description}}</p>
+      </div>
+      <div class="image">
+        <img :src=product.img>
+      </div>
+      <div class="price">
+        <h2>{{product.price}}</h2>
+        <button @click="addToCart(product)" class="auto">Add to Cart</button>
+      </div>
     </div>
+  </div>
+</div>
 </template>
+
 
 <script>
     export default {
@@ -39,72 +40,73 @@
     }
 </script>
 
+
 <style scoped>
-    .wrapper {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
-    .products {
-        margin-top: 20px;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-around;
-    }
+.products {
+  margin-top: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
 
-    .product {
-        margin: 10px;
-        margin-top: 50px;
-        width: 200px;
-    }
+.product {
+  margin: 10px;
+  margin-top: 50px;
+  width: 200px;
+}
 
-    .product img {
-        border: 2px solid #333;
-        height: 250px;
-        width: 200px;
-        object-fit: cover;
-    }
+.product img {
+  border: 2px solid #333;
+  height: 250px;
+  width: 200px;
+  object-fit: cover;
+}
 
-    .product .image {
-        display: flex;
-        justify-content: center;
-        margin-bottom: 5px;
-    }
+.product .image {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 5px;
+}
 
-    .info {
-        background: steelblue;
-        color: white;
-        padding: 10px 30px;
-        height: 80px;
-    }
+.info {
+  background: #F2921D;
+  color: #000;
+  padding: 10px 30px;
+  height: 100px;
+}
 
-    .info h1 {
-        font-size: 16px;
-    }
+.info h1 {
+  font-size: 16px;
+}
 
-    .info h2 {
-        font-size: 14px;
-    }
+.info h2 {
+  font-size: 14px;
+}
 
-    .info p {
-        margin: 0px;
-        font-size: 10px;
-    }
+.info p {
+  margin: 0px;
+  font-size: 10px;
+}
 
 
-    .price {
-        display: flex;
-    }
+.price {
+  display: flex;
+}
 
-    button {
-        height: 50px;
-        background: darkorange;
-        color: black;
-        border: none;
-    }
+button {
+  height: 50px;
+  background: #000;
+  color: white;
+  border: none;
+}
 
-    .auto {
-        margin-left: auto;
-    }
+.auto {
+  margin-left: auto;
+}
 </style>
